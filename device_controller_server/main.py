8b8dd87ddd2b6cae57598dfe47ca2767414c30cd
Yoo Hyun Sik <dbgustlr92@naver.com>
2017-08-27 17:48:24 +0900

    install 에러 통과
    apk crawler가 불안정하여 불완전한 apk파일 존재

--- device_controller_server/main.py
+++ device_controller_server/main.py
@@ -46,7 +46,7 @@ def main():
             # APK파일 하나씩 테스트 진행
             controller.run_test(apk)
         except Exception as e:
-            raise e
+            continue
             # 발생하는 Exception 종류가 뭐가있나?
 
 if __name__ == '__main__':
